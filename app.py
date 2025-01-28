ffe846cee88faed7a12f252ffaac173b741c5af6
Renato-MV <rromval599@fp.iesromerovargas.com>
2025-01-28 13:05:26 +0100

    Código ya revisado con Pylint.

--- app.py
+++ app.py
@@ -2,8 +2,6 @@
 
 
 def suma(a: int, b: int) -> int:
-    """ docstring function
-    # debe dar errores en linter
-    """
-    x = "no se usa"
-    return a+ b
+    """Comentario"""
+    # x = "no se usa"
+    return a + b
